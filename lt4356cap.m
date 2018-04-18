c41122f68261b662092b59ce10e229c88ee5d3a9
Keshuai Xu <urillx@gmail.com>
2018-04-17 21:27:00 -0400

    leak

--- lt4356cap.m
+++ lt4356cap.m
@@ -1,7 +1,9 @@
-function [v_out_dot, i_ds] = lt4356cap(t, v_out, v_in, i_lim, r_esr, c_out)
+function [v_out_dot, i_ds] = lt4356cap(t, v_out, v_in, i_lim, r_esr, c_out, r_leak)
 i_ds = (v_in - v_out) ./ r_esr;
 i_ds = min(i_lim, i_ds);
 
-v_out_dot = i_ds ./ c_out;
+i_leak = v_out ./ r_leak;
+
+v_out_dot = (i_ds - i_leak) ./ c_out;
 end
 
--- runner.m
+++ runner.m
@@ -2,8 +2,9 @@ v_in = 48;
 i_lim = 15;
 r_esr = 0.05;
 c_out = 1200e-6;
+r_leak = 100;
 
-odefun = @(t, y) lt4356cap(t, y, v_in, i_lim, r_esr, c_out);
+odefun = @(t, y) lt4356cap(t, y, v_in, i_lim, r_esr, c_out, r_leak);
 
 [t, V_OUT] = ode45(odefun, [0 0.01], 0);
 [V_DOT, I] = odefun(t, V_OUT);
